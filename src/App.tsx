3e64a848afc7b81dc6c5f681518ce474f20185c5
Pankyo(PK) Song <spkcome@gmail.com>
2026-07-15 11:34:48 -0500

    Add todo completion feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,62 @@
-import { useActionState, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useActionState, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
   const [task, setTask] = useState("");
   const [tasks, setTasks] = useState([]);
-  const [completed, setcompleted] = useState(false);
-  
+  // const [completed, setCompleted] = useState(false);
+
   return (
     <>
       <h1>PK's Todo App</h1>
-      <h2>Today's Task</h2>      
-      <h2>Count : {count}  </h2>
+      <h2>Today's Task</h2>
+      <h2>Count : {count} </h2>
       <button onClick={handleClick}>+1</button>
-      <input value = {task} onChange={handleChange}></input>
+      <input value={task} onChange={handleChange}></input>
       <h2>task : {task}</h2>
-      <button onClick ={addTask}>ADD List</button>      
-      <ul>{tasks.map((task,index) =><li key={index}>{task.text}<button onClick={() => deleteClick(index)}>DELETE</button></li>)} </ul>
-      <ul></ul>
+      <button onClick={addTask}>ADD List</button>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <input type="checkbox" onChange={() => toggleCheck(index)}></input>
+            {task.text}
+            <button onClick={() => deleteClick(index)}>DELETE</button>
+          </li>
+        ))}
+      </ul>
     </>
-  )
-  function handleClick(){
-      setCount(count+1);
-      console.log("count : "+ count);
+  );
+  function handleClick() {
+    setCount(count + 1);
+    console.log("count : " + count);
   }
 
-  function handleChange(event){
-      setTask(event.target.value);
-      // console.log("task 2 : "+ event.target.value);
+  function handleChange(event) {
+    setTask(event.target.value);
+    // console.log("task 2 : "+ event.target.value);
   }
 
-  function addTask(){
-    const taskObject ={ text : task, completed : false};
-    if(task.trim() == "") return;
-    setTasks([...tasks,taskObject]);
+  function addTask() {
+    const taskObject = { text: task, completed: false };
+    if (task.trim() == "") return;
+    setTasks([...tasks, taskObject]);
     setTask("");
   }
 
-  function deleteClick(index){
-    setTasks(tasks.filter((_,i)=> i != index));
+  function deleteClick(index) {
+    setTasks(tasks.filter((_, i) => i != index));
+  }
+
+  function toggleCheck(targetIndex) {
+    setTasks(
+      tasks.map((task, index) =>
+        index === targetIndex ? { ...task, completed: !task.completed } : task,
+      ),
+    );
   }
 }
-
-
-
-
-export default App
+export default App;
